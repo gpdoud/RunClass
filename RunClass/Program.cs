66a8ccc57fd5e812e69f611dc8de3e101f46534d
Greg Doud <gpdoud@gmail.com>
2024-06-11 16:38:58 -0400

    Add project files.

---
+++ RunClass/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace RunClass;
+
+internal class Program {
+    static void Main(string[] args) {
+
+        Hello hello = new Hello();
+
+        hello.Hi();
+
+    }
+}
+
+
+internal class Hello {
+    internal void Hi() {
+        Console.WriteLine("Hi!");
+    }
+}
